dbe79820e46003f209303f6778929ef7bf8d027f
thaissgarbosa <thais.ssgarbosa@gmail.com>
2025-03-12 09:24:42 -0300

    chore: create dummy content

--- app/(index,info)/index.tsx
+++ app/(index,info)/index.tsx
@@ -4,33 +4,75 @@ import * as AC from "@bacons/apple-colors";
 import { TextInput, View } from "react-native";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import { useColorScheme } from "react-native";
- 
-export default function Page() {
-    const colorScheme = useColorScheme();
 
-  return(
+export default function Page() {
+  const colorScheme = useColorScheme();
+
+  return (
     <View style={{ flex: 1 }}>
-      <Form.List navigationTitle = "Home" >
-        <Form.Section title = "Create post" footer = "Create a post to share with your friends">
+      <Form.List navigationTitle="Home">
+        <Form.Section
+          title="Create post"
+          footer="Create a post to share with your friends"
+        >
           <Form.HStack>
-            <TextInput placeholder  = "What's on your mind?" 
-            style={{
-              flexGrow: 1,
-              maxWidth: "80%",
-              borderWidth: 1,
-              borderColor: AC.separator,
-              borderRadius: 8,
-              padding: 12,
-              color: colorScheme === "dark" ? "white" : "black",
+            <TextInput
+              placeholder="What's on your mind?"
+              style={{
+                flexGrow: 1,
+                maxWidth: "80%",
+                borderWidth: 1,
+                borderColor: AC.separator,
+                borderRadius: 8,
+                padding: 12,
+                color: colorScheme === "dark" ? "white" : "black",
               }}
-              />
-             <Form.Link href = {"/(info)"}>
+            />
+            <Form.Link href={"/(info)"}>
               {/*@ts-ignore*/}
-              <IconSymbol name = "sparkle" color = {AC.systemOrange}/>
+              <IconSymbol name="sparkle" color={AC.systemOrange} />
             </Form.Link>
           </Form.HStack>
         </Form.Section>
-     </Form.List>
+        <Form.Section title="Recent Posts">
+          {dummyPost.map((post) => (
+            <Form.Link 
+            href={"/two"}
+            key={post.id} 
+            systemImage={post.isLiked ? "heart.fill" : "heart"}
+            style={{
+              overflow: "hidden",
+              flexShrink: 1,
+            }}
+            >
+              {post.text}
+            </Form.Link>
+          ))}
+        </Form.Section>
+      </Form.List>
     </View>
-  )
+  );
 }
+
+const dummyPost = [
+  // Se possuir chave é vetor
+
+  {
+    // Se possuir conchete é um objeto
+    id: 1,
+    text: "Just launched my new app!",
+    isLiked: false,
+  },
+
+  {
+    id: 2,
+    text: "Working on some new UI designs today.",
+    isLiked: true,
+  },
+
+  {
+    id: 3,
+    text: "Morning run complete!",
+    isLiked: true,
+  },
+];
